bcd716e9e7d4832d302160475e3d9b4e1cdb75bb
github username <github email address>
2018-12-12 20:31:07 +0000

    learn submit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+require 'pry'
 
+
+class CashRegister
+  attr_accessor :total, :discount, :prev_total, :arr
+
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @arr = []
+  end
+
+  def add_item(item,price,opt=1)
+    opt.times do self.arr << item; end 
+    self.prev_total = self.total
+    if self.total == 0
+      self.total += (price * opt)
+      self.prev_total = self.total
+    else
+      self.total += (price * opt)
+    end
+
+
+  end
+
+  def apply_discount
+    if self.discount != 0
+      self.total = self.total - self.total * (self.discount.to_f/100)
+
+      "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+
+    self.arr
+  end
+
+  def void_last_transaction
+
+    self.total = self.total - self.prev_total
+  end
+end
